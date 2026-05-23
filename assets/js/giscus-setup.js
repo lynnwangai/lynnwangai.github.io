c7455557aab7261b3772975c8104e71bf60ccc80
lynnwangai <30310335+lynnwangai@users.noreply.github.com>
2026-05-23 23:49:54 +0000

    Deploying to gh-pages from @ lynnwangai/lynnwangai.github.io@587804b890eba4f201c9e3d603eb93f71ea59961 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "lynnwangai/lynnwangai.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
